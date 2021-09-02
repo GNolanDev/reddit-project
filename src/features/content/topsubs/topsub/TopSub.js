3474ef64a633a5a9e3d4baeb5e271acde96b2400
gingergaz <gaz@cliffhanger.com>
2021-09-02 14:01:05 +0100

    Add click handler for selecting a subreddit

--- src/features/content/topsubs/topsub/TopSub.js
+++ src/features/content/topsubs/topsub/TopSub.js
@@ -1,9 +1,16 @@
+import { useDispatch } from "react-redux";
+import { setSelectedSubreddit } from "../../subreddit/subredditSlice";
 import "./TopSub.css";
 
 const TopSub = (props) => {
+  const dispatch = useDispatch();
   // onclick, dispatch action to change selected subreddit to props.url
+  const handleClick = (e) => {
+    dispatch(setSelectedSubreddit(props.url));
+  };
+
   return (
-    <button className="topSubContainer" onClick={() => {}}>
+    <button className="topSubContainer" onClick={handleClick}>
       <img src={props.icon_url} alt="" className="topSubIcon" />
       <div className="topSubName">{props.name}</div>
     </button>
