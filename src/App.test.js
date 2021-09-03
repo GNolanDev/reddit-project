dc60384efbe336b11b373157bae4b472408ecf28
gingergaz <gaz@cliffhanger.com>
2021-09-03 14:05:08 +0100

    Add test for clicking 2nd subreddit, mocking yet to add

--- src/App.test.js
+++ src/App.test.js
@@ -5,18 +5,48 @@ import App from "./App";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-// E2E tests
+// TODO: set up msw to intercept api requests
 
-describe("On loading the page", async () => {
+// E2E tests
+beforeEach(() => {
+  render(<App />);
+});
+
+describe("On loading the page", () => {
   it("should display a text input for searching", () => {
-    render(<App />);
     expect(
       screen.getByRole("textbox", { name: /search/i })
     ).toBeInTheDocument();
   });
-  it("should display the top subreddit of the day", async () => {
-    render(<App />);
+  it("should display the second subreddit of the day in list", async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
     const textElement = await screen.getByText(/ffxiv/);
     expect(textElement).toBeInTheDocument();
   });
+  it("should display the top subreddit of the day as the main content", async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    const textElement = await screen.getByText(/Showing posts from: Home/);
+    expect(textElement).toBeInTheDocument();
+  });
+  // tests for visible posts?
+});
+
+describe("On clicking the 2nd subreddit", () => {
+  it("should display the 2nd subreddit as the main content", async () => {
+    // click 2nd subreddit here
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    fireEvent.click(screen.getByText(/ffxiv/));
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+    const textElement = await screen.getByText(/Showing posts from: ffxiv/);
+    expect(textElement).toBeInTheDocument();
+  });
+  // tests for visible posts?
 });
