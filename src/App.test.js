c1c7f1d5628c142a766b4a353a3266ed6e86b438
gingergaz <gaz@cliffhanger.com>
2021-09-06 11:32:03 +0100

    Add tests for api fail or delay using msw request handler override

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { rest } from "msw";
 import { cleanup, render, fireEvent, screen } from "./test-utils/test-renderer";
 import App from "./App";
+import paths from "./constants/paths";
+const { base_url } = paths;
 import { server } from "./test-utils/test-server";
 import testdata from "./test-utils/test-data";
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // E2E tests
 // establish API mocking before all tests
@@ -11,7 +16,7 @@ beforeAll(() => {
 });
 // render the App within a Provider at teh start of each test
 beforeEach(() => {
-  render(<App />);
+  // render(<App />);
 });
 /* unmount components etc after each test to avoid leakage &
  * request handlers removed to ensure all tests are independent
@@ -27,27 +32,57 @@ afterAll(() => {
 
 describe("On loading the page", () => {
   it("should display a text input for searching", () => {
+    render(<App />);
     expect(
       screen.getByRole("textbox", { name: /search/i })
     ).toBeInTheDocument();
   });
   it("should display the second subreddit of the day in list", async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
+    render(<App />);
+    await wait(1000);
     const textElement = await screen.getByText(
       testdata.topsubs[1].data.display_name
     );
     expect(textElement).toBeInTheDocument();
   });
   it("should display the top subreddit of the day as the main content", async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    });
+    render(<App />);
+    await wait(1000);
     const textElement = await screen.getByText(
       `Showing posts from: ${testdata.topsubs[0].data.display_name}`
     );
     expect(textElement).toBeInTheDocument();
+  });
+  describe("When the subreddits fetch api has a problem", () => {
+    it("should display an error message if 404 returned", async () => {
+      server.use(
+        rest.get(`${base_url}/subreddits.json`, (req, res, ctx) => {
+          return res(
+            ctx.status(404),
+            ctx.json({ message: "Not Found", error: "404" })
+          );
+        })
+      );
+      render(<App />);
+      await wait(1000);
+      const textElement = await screen.getByText(/Could not fetch subreddits/i);
+      expect(textElement).toBeInTheDocument();
+    });
+    it("should display a waiting message if there's a delay", async () => {
+      server.use(
+        rest.get(`${base_url}/subreddits.json`, async (req, res, ctx) => {
+          await wait(4000);
+          return res(
+            ctx.status(404),
+            ctx.json({ message: "Not Found", error: "404" })
+          );
+        })
+      );
+      render(<App />);
+      await wait(1000);
+      const textElement = await screen.getByText(/fetching subreddits/i);
+      expect(textElement).toBeInTheDocument();
+    });
   });
   // tests for visible posts?
 });
