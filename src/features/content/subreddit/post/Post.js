fffb51a3cadca3d5673ffc6714764165e4d3ad3c
gingergaz <gaz@cliffhanger.com>
2021-09-07 16:46:14 +0100

    Begin adding Comment component

--- src/features/content/subreddit/post/Post.js
+++ src/features/content/subreddit/post/Post.js
@@ -2,14 +2,12 @@ import "./Post.css";
 
 const Post = (props) => {
   return (
-    <div className="postContainer" data-test={props.title}>
+    <div className="postContainer">
       <div className="postTitle">{props.title}</div>
       <div className="postImage">
         <img src={props.url} alt="" />
       </div>
-      <div className="postAuthor">
-        <div className="postAuthorName">{props.author}</div>
-      </div>
+      <div className="postAuthor">{props.author}</div>
     </div>
   );
 };
